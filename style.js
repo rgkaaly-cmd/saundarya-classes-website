bda06b6f9ff4cf56f77a5ddfe1efe7b2acb3d5d1
rgkaaly-cmd <rgkaaly@gmail.com>
2025-12-23 11:36:02 +0530

    Add files via upload

--- style.js
+++ style.js
@@ -3,7 +3,5 @@ const menu = document.querySelector('.menu');
 
 hamburger.addEventListener('click', () => {
     menu.classList.toggle('active');
-
-    // hamburger animation
     hamburger.classList.toggle('open');
 });
